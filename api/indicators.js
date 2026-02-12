c4d5cb676560c39c21aeb5ee9f3ded6bb4893c63
Bissegger <bisseggerkobi@gmail.com>
2026-02-12 20:22:27 +0100

    Update indicators.js

--- api/indicators.js
+++ api/indicators.js
@@ -1,10 +1,12 @@
 export function RSI(prices, period=14){
   let gains=0, losses=0;
-  for(let i=prices.length-period;i<prices.length;i++){
-    const diff=prices[i]-prices[i-1];
+
+  for(let i=1;i<=period;i++){
+    const diff = prices.at(-i)-prices.at(-i-1);
     if(diff>0) gains+=diff;
     else losses-=diff;
   }
-  const rs=gains/losses;
-  return 100-(100/(1+rs));
+
+  const rs = gains/(losses||1);
+  return 100 - (100/(1+rs));
 }
